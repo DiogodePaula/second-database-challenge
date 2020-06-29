8b340899f79a67fd6482e0dcbd2f9aaca03252be
DiogodePaula <depaulatrabalhos@gmail.com>
2020-06-29 19:26:56 -0300

    ...

--- index.js
+++ index.js
@@ -9,4 +9,35 @@ server.get("/", (req,res)=>{
     });
 });
 
+server.get("/notepad", async (req,res)=>{
+    let users;
+    await database.query(`SELECT * FROM notepad`, {type: database.QueryTypes.SELECT})
+    .then(resluts => {
+        users = results;
+    })
+    .catch(err =>{
+        return res.json("erro ao buscar note");
+    })
+    return res.json({users});
+});
+
+server.post("/notepad", async (req,res)=>{
+    let inseriu;
+    const {title, content, date, hour} = req.body;
+
+    await database.query(`INSERT INTO notepad VALUES(${id},'${title}', '${content}', '${date}', '${hour}');`,
+        {type: database.QueryTypes.INSERT})
+        .then(result =>{
+            inseriu = result
+        })
+        .catch(err =>{
+            return res.json(err);
+        });
+    if (inseriu[1]){
+        return res.json("note successfully inserted.");
+    } else {
+        return res.json("note could not be registered.");
+    }
+});
+
 server.listen(process.env.PORT);
